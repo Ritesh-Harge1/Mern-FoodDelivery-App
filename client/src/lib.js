1c61467bd7bbce3ded430f7eefaba65842a4407e
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-15 13:54:18 +0530

    Update lib.js

--- client/src/lib.js
+++ client/src/lib.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+const API = https://mern-fooddelivery-app.onrender.com
 
 export const api = axios.create({ baseURL: API })
 
